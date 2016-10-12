adbc300f742b6715a83aa2cb44a4be30dbc63624
Carl Schriever <Carl Schriever>
2016-10-11 22:57:12 -0400

    I'm not using hackerand anymore for this

---
+++ PE_2.js
@@ -0,0 +1,21 @@
+//Sum of even Fibonacci numbers beneath N
+
+function main() {
+
+    var n = 1000; //The number under which to find even Fibonacci numbers 
+    
+    var esum = 2;
+    
+    var last1 = 2;
+    var last2 = 1; 
+    
+    while(last1 < n){
+        var fsum = last1 + last2;
+        last2 = last1;
+        last1 = fsum;
+        if(fsum % 2 == 0 && fsum < n)esum += fsum;
+    }
+    
+    console.log(esum);
+
+}
